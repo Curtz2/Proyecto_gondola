db38014b75d90d53e3b929c8502618a237916e5c
Curtz2 <gradorjen45@gmail.com>
2024-05-25 00:23:36 -0500

    App
    Aplicacion Gondola

--- Gondola.py
+++ Gondola.py
@@ -1,38 +1,9 @@
-# streamlit_audio_recorder y whisper by Alfredo Diaz - version Mayo 2024
-
-# En VsC seleccione la version de Python (recomiendo 3.9) 
-#CTRL SHIFT P  para crear el enviroment (Escriba Python Create Enviroment) y luego venv 
-
-#o puede usar el siguiente comando en el shell
-#Vaya a "view" en el menú y luego a terminal y lance un terminal.
-#python -m venv env
-
-#Verifique que el terminal inicio con el enviroment o en la carpeta del proyecto active el env.
-#cd D:\flores\env\Scripts\
-#.\activate 
-
-#Debe quedar asi: (.venv) D:\proyectos_ia\Flores>
-
-#Puedes verificar que no tenga ningun libreria preinstalada con
-#pip freeze
-#Actualicie pip con pip install --upgrade pip
-
-#pip install tensorflow==2.15 La que tiene instalada Google Colab o con la versión qu fué entrenado el modelo
-#Verifique se se instaló numpy, no trate de instalar numpy con pip install numpy, que puede instalar una version diferente
-#pip install streamlit
-#Verifique se se instaló no trante de instalar con pip install pillow
-#Esta instalacion se hace si la requiere pip install opencv-python
-
-#Descargue una foto de una flor que le sirva de ícono 
-
-# importing the libraries and dependencies needed for creating the UI and supporting the deep learning models used in the project
 import os
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import streamlit as st  
-import tensorflow as tf # TensorFlow is required for Keras to work
+import tensorflow as tf
 from PIL import Image
 import numpy as np
-#import cv2
 
 # hide deprication warnings which directly don't affect the working of the application
 import warnings
@@ -41,53 +12,57 @@ warnings.filterwarnings("ignore")
 # set some pre-defined configurations for the page, such as the page title, logo-icon, page loading state (whether the page is loaded automatically or you need to perform some action for loading)
 st.set_page_config(
     page_title="Reconocimiento de Flores",
-    page_icon = ":smile:",
-    initial_sidebar_state = 'auto'
+    page_icon=":smile:",
+    initial_sidebar_state='auto'
 )
 
 # hide the part of the code, as this is just for adding some custom CSS styling but not a part of the main idea 
 hide_streamlit_style = """
-	<style>
-  #MainMenu {visibility: hidden;}
-	footer {visibility: hidden;}
-  </style>
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    </style>
 """
 
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) # Oculta el código CSS de la pantalla, ya que están incrustados en el texto de rebajas. Además, permita que Streamlit se procese de forma insegura como HTML
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
-#st.set_option('deprecation.showfileUploaderEncoding', False)
 @st.cache_resource
 def load_model():
-    model=tf.keras.models.load_model('./Gondola.h5')
+    model = tf.keras.models.load_model('./Gondola_model.h5')
     return model
-with st.spinner('Modelo está cargando..'):
-    model=load_model()
-    
 
+with st.spinner('Modelo está cargando..'):
+    model = load_model()
 
 with st.sidebar:
-        st.image('rosa.jpg')
-        st.title("Reconocimiento de imagen")
-        st.subheader("Reconocimiento de imagen para flores")
+    st.image('LWC.png')
+    st.title("Bienvenido a UNAB IA: Reconocimiento de Objetos de Góndola")
+    st.subheader("Reconocimiento de imágenes para productos de góndola")
 
-st.image('logo.jpg')
-st.title("Smart Regions Center")
-st.write("Somos un equipo apasionado de profesionales dedicados a hacer la diferencia")
+st.image('LWC.png')
+st.markdown("<h1 style='text-align: center;'>UNAB IA</h1>", unsafe_allow_html=True)
+
+# Texto centrado con partes en negrita
+html_text = """
+<div style="text-align: center;">
+    Nuestra aplicación utiliza tecnología avanzada de inteligencia artificial para <b>identificar y catalogar productos en góndolas</b> de manera rápida y precisa.
+    Simplifica la gestión de inventarios y mejora la experiencia de compra con nuestra innovadora herramienta de reconocimiento visual.
+    ¡Descubre una nueva forma de optimizar tu negocio con UNAB IA!
+</div>
+"""
+
+st.markdown(html_text, unsafe_allow_html=True)
+
 st.write("""
-         # Detección de flores
-         """
-         )
-
+         # Detección de productos
+         """)
 
 def import_and_predict(image_data, model, class_names):
-    
     image_data = image_data.resize((180, 180))
-    
     image = tf.keras.utils.img_to_array(image_data)
     image = tf.expand_dims(image, 0) # Create a batch
 
-    
-    # Predecir con el modelo
     prediction = model.predict(image)
     index = np.argmax(prediction)
     score = tf.nn.softmax(prediction[0])
@@ -95,23 +70,33 @@ def import_and_predict(image_data, model, class_names):
     
     return class_name, score
 
-
 class_names = open("./clases.txt", "r").readlines()
 
-img_file_buffer = st.camera_input("Capture una foto para identificar una flor")    
-if img_file_buffer is None:
-    st.text("Por favor tome una foto")
-else:
+st.write("Suba una foto o tome una nueva para identificar un producto")
+
+# Opciones de carga de imágenes
+img_file_buffer = st.file_uploader("Subir una imagen", type=["jpg", "jpeg", "png"])
+img_camera = st.camera_input("O tome una foto")
+
+image = None  # Inicializa la variable image
+
+# Determinar cuál entrada usar
+if img_file_buffer is not None:
     image = Image.open(img_file_buffer)
+elif img_camera is not None:
+    image = Image.open(img_camera)
+
+if image is not None:
     st.image(image, use_column_width=True)
     
     # Realizar la predicción
     class_name, score = import_and_predict(image, model, class_names)
     
     # Mostrar el resultado
-
-    if np.max(score)>0.5:
-        st.subheader(f"Tipo de Flor: {class_name}")
+    if np.max(score) > 0.5:
+        st.subheader(f"Tipo de producto: {class_name}")
         st.text(f"Puntuación de confianza: {100 * np.max(score):.2f}%")
     else:
-        st.text(f"No se pudo determinar el tipo de flor")
+        st.text("No se pudo determinar el tipo de producto")
+else:
+    st.text("Por favor, suba una imagen o tome una foto.")
